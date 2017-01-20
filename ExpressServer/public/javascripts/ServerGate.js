85027c4de4d94b9a0c14f5adca853357d6843e9c
Lee-jun-hee <wns8432@naver.com>
2017-01-20 19:53:16 +0900

    Express 세팅 및 80port Get요청 오면 Json 리턴 테스트

---
+++ ExpressServer/public/javascripts/ServerGate.js
@@ -0,0 +1,28 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+router.get('/testUserInfo', function (req, res) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    var json = JSON.stringify({
+        UserID : 'HIHI', 
+        Gold : '0', 
+    });
+    res.end(json);
+});
+
+router.get('/Login', function (req, res) {
+    
+});
+
+router.get('/Logout', function (req, res) {
+    
+});
+
+router.get('/SignIn', function (req, res) {
+    
+});
+
+
+
+module.exports = router;
--- ServerGate.js
+++ ServerGate.js
@@ -1,5 +1,5 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-    console.log("hi");
-});
+    console.log("Conncetion On 80 port");
+}).listien(80);
